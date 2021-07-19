d2dc428e7952c633587635ae8cf133f238092ca9
Ryo-not-rio <60484930+Ryo-not-rio@users.noreply.github.com>
2021-07-19 18:03:06 +0100

    fix notes

--- js/music-box.js
+++ js/music-box.js
@@ -1,5 +1,6 @@
 
 const gridWidth = 35;
+const numRows = 15;
 
 function placeNote(event) {
   const box = document.getElementById("music-box");
@@ -11,12 +12,17 @@ function placeNote(event) {
 
   const note = document.createElement("div");
   note.className = 'note quarter';
-  if (j < 3) {
-    note.style.gridRow = rowStart.toString() + '/' + (rowStart+2).toString();
-    note.style.transform = "scaleY(-1) translate(0px,-5px)";
+  if (j==0 || j==numRows-1) {
+    note.className += ' line';
+  }
+  const addNum = 4; // Additional spaces to occupy to render note
+  if (j < 6) {
+    // Upside-down
+    note.style.gridRow = rowStart.toString() + '/' + (rowStart+addNum).toString();
+    note.style.transform = "scaleY(-1) scaleX(-1) translate(0px,0px)";
   } else {
-    note.style.gridRow = (rowStart-2).toString() + '/' + rowStart.toString();
-    note.style.transform = "translate(0px,27px)";
+    note.style.gridRow = (rowStart-addNum).toString() + '/' + rowStart.toString();
+    note.style.transform = "translate(0px,15px)";
   }
 
   note.style.gridColumn = (i+1).toString() + '/' + (i+2).toString();
@@ -27,10 +33,9 @@ function makeGrid() {
   const box = document.getElementById("music-box");
   
   const numCols = Math.floor(box.offsetWidth/gridWidth);
-  const numRows = 7;
 
   box.style.gridTemplateColumns = 'repeat(' + numCols.toString() + ', 1fr)'
-  box.style.gridTemplateRows = 'repeat(' + numRows.toString() + ', 1fr)'
+  box.style.gridTemplateRows = 'repeat(' + numRows.toString() + ', ' + Math.floor(box.offsetHeight/numRows).toString() + 'px)'
   for (let i=0; i<numCols; i++) {
     for (let j=0; j<numRows; j++) {
       const gridContainer = document.createElement("div");
@@ -44,9 +49,9 @@ function makeGrid() {
       const grid = document.createElement("canvas");
       grid.className = 'grid-square';
       
-      if (j > 0 && j < 6) {
+      if (j > 2 && j < 12 && j%2 == 1) {
         ctx = grid.getContext("2d");
-        ctx.lineWidth = 3;
+        ctx.lineWidth = 10;
         ctx.beginPath();
         ctx.moveTo(0, Math.floor(grid.height/2));
         ctx.lineTo(grid.width, Math.floor(grid.height/2));
